86f2a4ecfa6aae56a9d1dfe0ce1805e8b799ef4e
vardhan ganugula <gvardhan2004@gmail.com>
2024-12-23 17:40:12 +0530

    production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Enables strict mode for React
+  swcMinify: true,       // Enables SWC for faster builds
+  
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during builds
+  },
+
 };
 
 export default nextConfig;
